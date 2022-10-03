b6dd6ff87418c07af8eb0f53e533ff8fe3bd41bd
Grigory Stepanov <grigory-stepanoff@yandex.ru>
2022-10-03 20:35:02 +0400

    calling by name

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 
 int main()
 {
-    printf("henlo!");
+    char name [20] = "";
+    printf ("Please enter your name:\n");
+    scanf ("%s", name);
+    printf ("henlo, %s!\n", name);
 
     return 0;
 }
