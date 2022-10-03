a7d93315d2c72a59b73f5a1d4c379d3774b139af
Grigory Stepanov <grigory-stepanoff@yandex.ru>
2022-10-03 20:42:44 +0400

    age recognition added

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@ int main()
     char name [20] = "";
     printf ("Please enter your name:\n");
     scanf ("%s", name);
-    printf ("henlo, %s!\n", name);
+
+    int age = 0;
+    printf ("Please tell me also your age:\n");
+    scanf ("%d", &age);
+
+    printf ("henlo, %s!, whose age is %d\n", name, age);
 
     return 0;
 }
